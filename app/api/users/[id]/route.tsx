907088e8a0ba5e9c50c21fa23730b0ee2b5991f5
Fabio Girones <fdgirones@gmail.com>
2024-04-08 13:54:16 +0200

    Added a prisma database

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -4,12 +4,12 @@ import schema from "../schema";
 
 export function GET(
     request: NextRequest, 
-    {params}: {params: {id: number}}) {
+    {params}: {params: {id: string}}) {
     // Fetch data from a db
     // If not found, return 404 error
     // Else return data
 
-    if (params.id > 10)
+    if (parseInt(params.id) > 10)
         return NextResponse.json({error: 'User not found'}, {status: 404})
 
     return NextResponse.json({id: 1, name: "Mosh"})
